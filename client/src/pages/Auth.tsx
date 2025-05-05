59eb0790a9d68e7b691229b843724c05be7deaa6
sharathbisha <sharath.bhopal@ishafoundation.org>
2025-05-05 10:56:43 +0530

    Add auth page images and update layout with responsive image grid

--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -1,19 +1,54 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Image, Flex } from '@chakra-ui/react';
 import AuthForm from '../components/ui/auth/AuthForm';
+import authImage1 from '../assets/authPageImage1.webp';
+import authImage2 from '../assets/authPageImage2.webp';
+import authImage3 from '../assets/authPageImage3.webp';
 
 export default function Auth() {
 
   return (
     <Box
       minH="100vh"
+      minW="100vw"
       bgGradient="linear(to-b, blue.50, white)"
       display="flex"
-      alignItems="center"
-      justifyContent="center"
+      alignItems="space-between"
+      justifyContent="space-between"
       p={8}
       pt={0}
+      gap={8}
     >
-      <AuthForm />
+      <Flex direction={{ base: 'column', md: 'row' }} align="center" justify="center" gap={8}
+      >
+        <Box m={4}>
+          <AuthForm />
+        </Box>
+        <Flex align="space-between" justify="center" wrap="wrap" >
+          <Image
+            src={authImage1}
+            alt="Neighbour Stores"
+            maxW={{ base: '100%', md: '500px' }}
+            borderRadius="lg"
+            boxShadow="lg"
+            mr={4}
+          />
+          <Image
+            src={authImage3}
+            alt="Neighbour Stores"
+            maxW={{ base: '100%', md: '500px' }}
+            borderRadius="lg"
+            boxShadow="lg"
+          />
+
+          <Image
+            src={authImage2}
+            alt="Neighbour Stores"
+            maxW={{ base: '100%', md: '500px' }}
+            borderRadius="lg"
+            boxShadow="lg"
+          />
+        </Flex>
+      </Flex>
     </Box>
   );
 };
